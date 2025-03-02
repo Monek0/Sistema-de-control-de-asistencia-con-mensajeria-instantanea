e4735341afdfc546650629e64e0f43a002779297
Juan José Henao Quintero <126366087+Monek0@users.noreply.github.com>
2025-03-02 17:13:23 -0300

    Update authService.js

--- Plataforma_Atrasos/frontend/src/services/authService.js
+++ Plataforma_Atrasos/frontend/src/services/authService.js
@@ -1,5 +1,5 @@
 export const login = async (rutUsername, contraseña) => {
-    const response = await fetch('http://localhost:3000/auth/login', {
+    const response = await fetch('https://dr6mlrzheooq5qgtd2b3nddala0cwyxg.lambda-url.us-east-2.on.aws/auth/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -18,7 +18,7 @@ export const login = async (rutUsername, contraseña) => {
 
 export const register = async ({ nombreUsuario, rutUsername, contraseña, codRol }) => {
     try {
-        const response = await fetch('http://localhost:3000/auth/register', {
+        const response = await fetch('https://dr6mlrzheooq5qgtd2b3nddala0cwyxg.lambda-url.us-east-2.on.aws/auth/register', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
