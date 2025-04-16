db507b1379cdd09e42ea1045c30ec6f9d1897399
Monek0 <jjsflepaquintero@gmail.com>
2025-04-15 23:07:49 -0400

    limpieza

--- Plataforma_Atrasos/__mocks__/imageMocks.js
+++
@@ -1,5 +0,0 @@
-// frontend/src/__mocks__/imageMocks.js
-export const controlIcon = 'control-icon-mock';
-export const reportIcon = 'report-icon-mock';
-export const messageIcon = 'message-icon-mock';
-export const agregarIcon = 'agregar-usuario-icon-mock';
--- Plataforma_Atrasos/__mocks__/whatsapp-web.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    Client: jest.fn(() => ({
-        on: jest.fn(),
-        initialize: jest.fn(),
-        sendMessage: jest.fn(() => Promise.resolve()), // Simula envío exitoso
-        destroy: jest.fn(),
-    })),
-};
